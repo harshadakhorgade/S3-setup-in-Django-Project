c6b74eb5eff09055c2df3fdd2822a416e32dccb5
harshadakhorgade <harshadakhorgade9@gmail.com>
2024-12-22 14:16:24 +0530

    updated

--- backend/settings.py
+++ backend/settings.py
@@ -84,13 +84,24 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 #Replace name, Password, Host with your RDS Database name, user, password, host.
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'mydatabase',
+#         'USER': 'mydatabaseuser',
+#         'PASSWORD': 'mypassword',
+#         'HOST': 'myrdshost.rds.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydatabase',
-        'USER': 'mydatabaseuser',
-        'PASSWORD': 'mypassword',
-        'HOST': 'myrdshost.rds.amazonaws.com',
+        'NAME': 'mydemo',
+        'USER': 'superuser',
+        'PASSWORD': 'mysuperuser',
+        'HOST': 'mydemo.c1asac4y4rwk.ap-south-1.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
@@ -150,12 +161,13 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
-AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID '
-AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
-AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
-AWS_S3_SIGNATURE_NAME = 's3v4',
-AWS_S3_REGION_NAME = 'AWS_S3_REGION_NAME'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL =  None
-AWS_S3_VERIFY = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# AWS_ACCESS_KEY_ID = 'AWS_ACCESS_KEY_ID '
+# AWS_SECRET_ACCESS_KEY = 'AWS_SECRET_ACCESS_KEY'
+# AWS_STORAGE_BUCKET_NAME = 'AWS_STORAGE_BUCKET_NAME'
+# AWS_S3_SIGNATURE_NAME = 's3v4',
+# AWS_S3_REGION_NAME = 'AWS_S3_REGION_NAME'
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL =  None
+# AWS_S3_VERIFY = True
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
